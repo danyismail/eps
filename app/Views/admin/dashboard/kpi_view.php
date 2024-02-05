e1acf30852b60d368fe43f60203bb61228921ecf
danyismail <dany_arie@yahoo.com>
2024-02-05 13:23:05 +0000

    changes in prod

--- app/Views/admin/dashboard/kpi_view.php
+++ app/Views/admin/dashboard/kpi_view.php
@@ -1,30 +1,44 @@
 <?php $this->extend('admin/layout/template') ?>
 <?php $this->Section('content') ?>
-  <div class="pr-5 pl-5 mt-5">
+  <div class="pr-5 pl-5 mt-2">
     <div class="row">
       <div class="col-md-12">
         <form method="GET" action="<?=base_url('')?>" class="mb-5">
-          <!-- <div class="form-row"> -->
-            <div class="row">
-              <div class="form-group col-md-3">
-                <label for="StartDate">Start Date <?=isset($startDt) ?? ''?></label>
-                <input type="date" name="startDt" class="form-control" value="<?=isset($startDt) ?? ''?>" />
+              <div class="row mb-4">
+                <div class="form-group col-md-3">
+                  <label for="StartDate">Start Date</label>
+                  <input type="date" name="startDt" class="form-control" value="<?=@$_GET['startDt']?>" />
+                </div>
+                <div class="form-group col-md-3">
+                  <label for="endDt">End Date</label>
+                  <input type="date" name="endDt" class="form-control" value="<?=@$_GET['endDt']?>" />
+                </div>
+                <div class="form-group col-md-3">
+                  <label for="mdn">MDN</label>
+                  <input type="text" name="mdn" class="form-control" value="<?=@$_GET['mdn']?>" />
+                </div>
               </div>
-              <div class="form-group col-md-3">
-                <label for="endDt">End Date</label>
-                <input type="date" name="endDt" class="form-control" value="<?=isset($endDate) ?? ''?>"/>
+              <div class="row">
+                <div class="form-group col-md-3">
+                  <label for="shift">Shift</label>
+                  <select name="shift" class="form-control">
+                    <option value="">-- Choose --</option>
+                    <option value="pagi" <?=@$_GET['shift'] === "pagi" ? "selected" : ''?>>Pagi</option>
+                    <option value="siang" <?=@$_GET['shift'] === "siang" ? "selected" : ''?>>Siang</option>
+                    <option value="malam" <?=@$_GET['shift'] === "malam" ? "selected" : ''?>>Malam</option>
+                  </select>
+                </div>
+                <div class="form-group col-md-3">
+                  <label for="status">Status</label>
+                  <select name="status" class="form-control">
+                    <option value="">-- Choose --</option>
+                    <option value="40" <?=@$_GET['status'] === "40" ? "selected" : ''?>>Gagal</option>
+                    <option value="52" <?=@$_GET['status'] === "52" ? "selected" : ''?>>Tujuan Salah</option>
+                    <option value="20" <?=@$_GET['status'] === "20" ? "selected" : ''?>>Sukses</option>
+                  </select>
+                </div>
               </div>
-              <div class="form-group col-md-3">
-                <label for="StartDate">MDN <?=isset($startDt) ?? ''?></label>
-                <input type="text" name="mdn" class="form-control" value="<?=isset($startDt) ?? ''?>" />
-              </div>
-              <div class="form-group col-md-3">
-                <label for="endDt">Status</label>
-                <input type="text" name="status" class="form-control" value="<?=isset($endDate) ?? ''?>"/>
-              </div>
-            </div>
-            <button type="submit" class="btn btn-primary mt-2">Submit</button>
-          <!-- </div> -->
+            <button type="submit" class="btn btn-primary mt-4">Submit</button>
         </form>
       </div>
     </div>
@@ -46,21 +60,23 @@
             <?php foreach($data as $row): ?>
                 <tr>
                 <td><?=date("d-m-Y h:m:s", strtotime($row['tanggal_entri']))?></td>
-                    <td><?=date("d-m-Y h:m:s", strtotime($row['tanggal_status']))?></td>
-                    <?php if($row['status'] == "40") {
-                        echo "<td style='color:red;'>gagal</td>";
+                <td><?=date("d-m-Y h:m:s", strtotime($row['tanggal_status']))?></td>
+                <td>
+                  <?php if($row['status'] == "40") {
+                        echo "<span class='text-danger'>GAGAL</span>";
                     } elseif ($row['status'] == "52"){
-                      echo "<td style='color:orange;'>tujuan salah</td>";
+                        echo "<span class='text-warning'>TUJUAN SALAH</span>";
                     } else {
-                      echo "<td style='color:green;'>sukses</td>";
+                        echo "<span class='text-success'>SUKSES</span>";
                     }
-                    ?>
-                    <td><?=$row['kode_produk']?></td>
-                    <td><?=$row['tujuan']?></td>
-                    <td <?php if($row['kpi'] > 180): ?> style="color:red;" <?php endif; ?>>
-                        <?php echo $row['waktu_respon']?>
-                    </td>
-                    <td><?=$row['shift']?></td>
+                  ?>
+                </td>
+                  <td><?=$row['kode_produk']?></td>
+                  <td><?=$row['tujuan']?></td>
+                  <td <?php if($row['kpi'] > 180): ?> style="color:red;" <?php endif; ?>>
+                      <?php echo $row['waktu_respon']?>
+                  </td>
+                  <td><?=$row['shift']?></td>
                 </tr>
             <?php endforeach ?>
         </tbody>
