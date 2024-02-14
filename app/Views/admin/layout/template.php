00864ea046bd2d95d6ea5f21651be082f83e5736
Babay halimi <babay.halimi@smartfren.com>
2024-02-14 18:57:44 +0700

    update

--- app/Views/admin/layout/template.php
+++ app/Views/admin/layout/template.php
@@ -24,7 +24,7 @@
 <link rel="stylesheet" href=<?=base_url('assets/vendors/material_icon/material-icons.css')?> />
 <link rel="stylesheet" href=<?=base_url('assets/css/metisMenu.css')?>>
 <link rel="stylesheet" href=<?=base_url('assets/css/style1.css')?> />
-<link rel="stylesheet" href=<?=base_url('assets/css/colors/default.css" id="colorSkinCSS')?>>
+<link rel="stylesheet" href=<?=base_url('assets/css/colors/default.css')?>>
 <style>
     .pagination {
         list-style-type: none;
@@ -178,33 +178,33 @@
 
 
 
-<script src=<?php base_url('assets/js/jquery1-3.4.1.min.js')?>></script>
-<script src=<?php base_url('assets/js/popper1.min.js')?>></script>
-<script src=<?php base_url('assets/js/bootstrap1.min.js')?>></script>
-<script src=<?php base_url('assets/js/metisMenu.js')?>></script>
-<script src=<?php base_url('assets/vendors/count_up/jquery.waypoints.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/chartlist/Chart.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/count_up/jquery.counterup.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/swiper_slider/js/swiper.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/niceselect/js/jquery.nice-select.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/owl_carousel/js/owl.carousel.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/gijgo/gijgo.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/jquery.dataTables.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/dataTables.responsive.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/dataTables.buttons.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/buttons.flash.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/jszip.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/pdfmake.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/vfs_fonts.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/buttons.html5.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/datatable/js/buttons.print.min.js')?>></script>
-<script src=<?php base_url('assets/js/chart.min.js')?>></script>
-<script src=<?php base_url('assets/vendors/progressbar/jquery.barfiller.js')?>></script>
-<script src=<?php base_url('assets/vendors/tagsinput/tagsinput.js')?>></script>
-<script src=<?php base_url('assets/vendors/text_editor/summernote-bs4.js')?>></script>
-<script src=<?php base_url('assets/vendors/apex_chart/apexcharts.js')?>></script>
-<script src=<?php base_url('assets/js/custom.js')?>></script>
-<script src=<?php base_url('assets/vendors/apex_chart/bar_active_1.js')?>></script>
-<script src=<?php base_url('assets/vendors/apex_chart/apex_chart_list.js')?>></script>
+<script src=<?=base_url('assets/js/jquery1-3.4.1.min.js')?>></script>
+<script src=<?=base_url('assets/js/popper1.min.js')?>></script>
+<script src=<?=base_url('assets/js/bootstrap1.min.js')?>></script>
+<script src=<?=base_url('assets/js/metisMenu.js')?>></script>
+<script src=<?=base_url('assets/vendors/count_up/jquery.waypoints.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/chartlist/Chart.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/count_up/jquery.counterup.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/swiper_slider/js/swiper.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/niceselect/js/jquery.nice-select.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/owl_carousel/js/owl.carousel.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/gijgo/gijgo.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/jquery.dataTables.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/dataTables.responsive.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/dataTables.buttons.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/buttons.flash.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/jszip.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/pdfmake.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/vfs_fonts.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/buttons.html5.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/datatable/js/buttons.print.min.js')?>></script>
+<script src=<?=base_url('assets/js/chart.min.js')?>></script>
+<script src=<?=base_url('assets/vendors/progressbar/jquery.barfiller.js')?>></script>
+<script src=<?=base_url('assets/vendors/tagsinput/tagsinput.js')?>></script>
+<script src=<?=base_url('assets/vendors/text_editor/summernote-bs4.js')?>></script>
+<script src=<?=base_url('assets/vendors/apex_chart/apexcharts.js')?>></script>
+<script src=<?=base_url('assets/js/custom.js')?>></script>
+<script src=<?=base_url('assets/vendors/apex_chart/bar_active_1.js')?>></script>
+<script src=<?=base_url('assets/vendors/apex_chart/apex_chart_list.js')?>></script>
 </body>
 </html>
