3185b910d18fd043a8837de8a09763f1ba2f651b
Babay halimi <babay.halimi@smartfren.com>
2024-02-26 22:44:55 +0700

    add module supplier

---
+++ app/Controllers/Amz/Supplier.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Controllers\Amz;
+use App\Controllers\BaseController;
+
+class Supplier extends BaseController
+{
+    public function index()
+	{
+        $request = request();
+        $client = service('curlrequest');
+
+        $posts_data = $client->request("GET", getenv('API_HOST')."/api/finance/amz/supplier", [
+			"headers" => [
+				"Accept" => "application/json",
+                "Content-Type" => "application/json"
+			],
+		]);
+
+        $res = json_decode($posts_data->getBody(), true);
+        $response['data'] = $res['data'] ?? array();
+        $response['breadcrumb'] = array(
+            array('label' => 'Home', 'url' => '', 'active' => false),
+            array('label' => 'Supplier', 'url' => '', 'active' => true)
+        );
+
+        echo view('admin/Amz/supplier/view', $response);
+	}
+
+    public function add() {
+        $response['breadcrumb'] = array(
+            array('label' => 'Home', 'url' => '', 'active' => false),
+            array('label' => 'Supplier', 'url' => '/amz/supplier', 'active' => false),
+            array('label' => 'Add', 'url' => '', 'active' => true)
+        );
+        echo view('admin/Amz/supplier/create', $response);
+    }
+
+    public function create() {
+        $request = request();
+        $client = service('curlrequest');
+
+        $dataPost['name'] = $request->getPost('name');
+        $dataPost['status'] = $request->getPost('status');
+
+        $posts_data = $client->request("POST", getenv('API_HOST')."/api/finance/amz/supplier/create", [
+			"headers" => [
+				"Accept" => "application/json",
+                "Content-Type" => "application/json"
+			],
+            "form_params" => $dataPost
+		]);
+        return redirect()->to('/amz/supplier');
+    }
+
+    public function status() {
+        $request = request();
+        $client = service('curlrequest');
+
+        $dataPost['id'] = $request->getGet('id');
+        $dataPost['status'] = $request->getGet('q');
+
+        $posts_data = $client->request("POST", getenv('API_HOST')."/api/finance/amz/supplier/update", [
+			"headers" => [
+				"Accept" => "application/json",
+                "Content-Type" => "application/json"
+			],
+            "form_params" => $dataPost
+		]);
+        return redirect()->to('/amz/supplier');
+    }
+
+    public function edit() {
+        $request = request();
+        $client = service('curlrequest');
+
+        $id = $request->getGet('id');
+
+        $getAPI = $client->request("GET", getenv('API_HOST')."/api/finance/amz/supplier/$id", [
+			"headers" => [
+				"Accept" => "application/json",
+                "Content-Type" => "application/json"
+			],
+		]);
+        $res = json_decode($getAPI->getBody(), true);
+        $response['data'] = $res['data'] ?? array();
+        $response['breadcrumb'] = array(
+            array('label' => 'Home', 'url' => '', 'active' => false),
+            array('label' => 'Supplier', 'url' => '/amz/supplier', 'active' => false),
+            array('label' => 'Edit', 'url' => '', 'active' => true)
+        );
+
+        echo view('admin/Amz/supplier/update', $response);
+    }
+
+    public function update() {
+        $request = request();
+        $client = service('curlrequest');
+
+        $dataPost['id'] = $request->getPost('id');
+        $dataPost['status'] = $request->getPost('status');
+
+        $posts_data = $client->request("POST", getenv('API_HOST')."/api/finance/amz/supplier/update", [
+			"headers" => [
+				"Accept" => "application/json",
+                "Content-Type" => "application/json"
+			],
+            "form_params" => $dataPost
+		]);
+
+        return redirect()->to('/amz/supplier');
+    }
+
+    public function delete() {
+        $request = request();
+        $client = service('curlrequest');
+
+        $id = $request->getGet('id');
+
+        $posts_data = $client->request("DELETE", getenv('API_HOST')."/api/finance/amz/supplier/delete/$id");
+
+        return redirect()->to('/amz/supplier');
+    }
+}
