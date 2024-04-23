24cc8849dcd0a7d101902837a190cc98e8f6eeec
babay.halimi <babay.halimi@smartfren.com>
2024-04-23 21:59:56 +0700

    update assets template

--- app/Views/admin/layout/template.php
+++ app/Views/admin/layout/template.php
@@ -6,27 +6,11 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>epsxpay.com</title>
-    <link rel="icon" href="assets/img/logo.png" type="image/png">
-
     <link rel="stylesheet" href=<?=base_url('assets/css/bootstrap1.min.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/themefy_icon/themify-icons.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/swiper_slider/css/swiper.min.css')?> />
     <link rel="stylesheet" href=<?=base_url('assets/vendors/select2/css/select2.min.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/niceselect/css/nice-select.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/owl_carousel/css/owl.carousel.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/gijgo/gijgo.min.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/font_awesome/css/all.min.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/tagsinput/tagsinput.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/datatable/css/jquery.dataTables.min.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/datatable/css/responsive.dataTables.min.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/datatable/css/buttons.dataTables.min.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/text_editor/summernote-bs4.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/morris/morris.css')?>>
-    <link rel="stylesheet" href=<?=base_url('assets/vendors/material_icon/material-icons.css')?> />
-    <link rel="stylesheet" href=<?=base_url('assets/css/metisMenu.css')?>>
     <link rel="stylesheet" href=<?=base_url('assets/css/style1.css')?> />
     <link rel="stylesheet" href=<?=base_url('assets/css/colors/default.css')?>>
-
+    <link rel="stylesheet" href=<?=base_url('assets/vendors/font_awesome/css/all.min.css')?> />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
@@ -184,36 +168,10 @@
     </div> -->
     </section>
 
-
-
     <script src=<?=base_url('assets/js/jquery1-3.4.1.min.js')?>></script>
     <script src=<?=base_url('assets/js/popper1.min.js')?>></script>
     <script src=<?=base_url('assets/js/bootstrap1.min.js')?>></script>
     <script src=<?=base_url('assets/js/metisMenu.js')?>></script>
-    <script src=<?=base_url('assets/vendors/count_up/jquery.waypoints.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/chartlist/Chart.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/count_up/jquery.counterup.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/swiper_slider/js/swiper.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/niceselect/js/jquery.nice-select.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/owl_carousel/js/owl.carousel.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/gijgo/gijgo.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/jquery.dataTables.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/dataTables.responsive.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/dataTables.buttons.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/buttons.flash.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/jszip.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/pdfmake.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/vfs_fonts.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/buttons.html5.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/datatable/js/buttons.print.min.js')?>></script>
-    <script src=<?=base_url('assets/js/chart.min.js')?>></script>
-    <script src=<?=base_url('assets/vendors/progressbar/jquery.barfiller.js')?>></script>
-    <script src=<?=base_url('assets/vendors/tagsinput/tagsinput.js')?>></script>
-    <script src=<?=base_url('assets/vendors/text_editor/summernote-bs4.js')?>></script>
-    <script src=<?=base_url('assets/vendors/apex_chart/apexcharts.js')?>></script>
-    <script src=<?=base_url('assets/js/custom.js')?>></script>
-    <script src=<?=base_url('assets/vendors/apex_chart/bar_active_1.js')?>></script>
-    <script src=<?=base_url('assets/vendors/apex_chart/apex_chart_list.js')?>></script>
     <script src="<?=base_url('assets/vendors/select2/js/select2.min.js')?>"></script>
     <script>
       $("#singleSelect").select2();
